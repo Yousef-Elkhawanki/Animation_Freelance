45bb116ee7e065e585d666a135d8993b082fecd4
frontend-eraasoft <yousef.eraasoft@gmail.com>
2023-06-03 22:44:47 +0300

    add feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,26 @@
 // Global Variables 
 const images__list = document.querySelectorAll('.image__container img');
-let manageAfterText = document.createElement("style");
+const text__container = document.querySelector('.text__container h6');
+const text__box = document.querySelector('.text__container');
 let loading__page__counter = document.querySelector('.loading__page span');
 let counter = 0
+// stop hover event on text 
+text__box.style.pointerEvents = "none"; 
 
 
 // Event when Move In Or Out All Pictures 
 for (let i = 0; i < images__list.length; i++) {
     images__list[i].addEventListener('mouseenter' ,(e)=>{
-        let textContent = e.target.getAttribute ('alt')
-        let manageAfterText = document.createElement("style");
-manageAfterText.innerHTML =
-` .text__container::after {
-    content: "";
-    width: 100%;
-    height: 100%;
-    text-transform: capitalize;
-    position: absolute;
-    z-index: 6;
-    opacity: 1;
-    transition: 0.5s;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }`;
-        document.head.appendChild(manageAfterText);
-        manageAfterText.innerHTML = manageAfterText.innerHTML.replace('content: ""', `content:"${textContent}"`)
+        let textContent = e.target.getAttribute ('alt');
+        text__container.textContent = textContent
+        text__container.style.opacity = `1`
     });
     images__list[i].addEventListener('mouseleave' ,(e)=>{
         console.log('leave');
-        let textContent = e.target.getAttribute ('alt')
-        let manageAfterText = document.createElement("style");
-        manageAfterText.innerHTML =
-        ` .text__container::after {
-            content: "";
-            width: 100%;
-            height: 100%;
-            text-transform: capitalize;
-            position: absolute;
-            z-index: 6;
-            opacity: 1;
-            transition: 0.5s;
-            text-align: center;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }`;
-                document.head.appendChild(manageAfterText);
-        manageAfterText.innerHTML = manageAfterText.innerHTML.replace(`content:"${textContent}"`, `content:"${textContent}"`)
+        let textContent = e.target.getAttribute ('alt');
+        text__container.textContent = ''
     });
-}
+};
 
 
 
@@ -61,18 +31,19 @@ let counter_Interval = setInterval( function(){
     if (counter == 100){
         clearInterval(counter_Interval)
         $(document).ready(()=>{
-            $('.loading__container').fadeOut(1000 ,function (){
-                $('.loading__page').slideUp(1000 ,function (){
+            $('.loading__container').fadeOut(1000 , ()=>{
+                $('.loading__page').slideUp(1000 , ()=>{
                     $('.welcome__message').fadeOut(2000 ,()=>{
                         $('.welcome__message').remove()
                         $('.loading__page').remove()
                     })
-                    $('.hero__banner').animate ({top:'0rem'} ,1000)
+                    $('.hero__banner').animate ({top:'0rem'} )
+                    
                 })
             });
         })
     }
-}, 200)
+}, 200);
 
 
 
