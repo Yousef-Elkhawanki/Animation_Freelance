1da786fe2a188bde07cee965446aea833ce3e279
frontend-eraasoft <yousef.eraasoft@gmail.com>
2023-06-04 19:25:30 +0300

    last update

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,15 +10,46 @@ text__box.style.pointerEvents = "none";
 
 // Event when Move In Or Out All Pictures 
 for (let i = 0; i < images__list.length; i++) {
+    // when Enter Images 
     images__list[i].addEventListener('mouseenter' ,(e)=>{
         let textContent = e.target.getAttribute ('alt');
-        text__container.textContent = textContent
-        text__container.style.opacity = `1`
+        if (textContent == $('.text__container h6')[0].textContent){
+            console.log($('.text__container h6:nth-child(1)'));
+            $('.text__container h6:nth-child(1)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        }
+        else if (textContent == $('.text__container h6')[1].textContent){
+            $('.text__container h6:nth-child(2)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        }
+        else if (textContent == $('.text__container h6')[2].textContent){
+            $('.text__container h6:nth-child(3)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        }
+        else if (textContent == $('.text__container h6')[3].textContent){
+            $('.text__container h6:nth-child(4)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        }
+        else if (textContent == $('.text__container h6')[4].textContent){
+            $('.text__container h6:nth-child(5)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        }
     });
+    
+        // when Out Images 
     images__list[i].addEventListener('mouseleave' ,(e)=>{
-        console.log('leave');
         let textContent = e.target.getAttribute ('alt');
-        text__container.textContent = ''
+        if (textContent == $('.text__container h6')[0].textContent){
+            console.log($('.text__container h6:nth-child(1)'));
+            $('.text__container h6:nth-child(1)').css({opacity:'0', transform:'translate(-50% , 0%)'})
+        }
+        else if (textContent == $('.text__container h6')[1].textContent){
+            $('.text__container h6:nth-child(2)').css({opacity:'0', transform:'translate(-50% , 0%)'})
+        }
+        else if (textContent == $('.text__container h6')[2].textContent){
+            $('.text__container h6:nth-child(3)').css({opacity:'0', transform:'translate(-50% , 0%)'})
+        }
+        else if (textContent == $('.text__container h6')[3].textContent){
+            $('.text__container h6:nth-child(4)').css({opacity:'0', transform:'translate(-50% , 0%)'})
+        }
+        else if (textContent == $('.text__container h6')[4].textContent){
+            $('.text__container h6:nth-child(5)').css({opacity:'0', transform:'translate(-50% , 0%)'})
+        }
     });
 };
 
@@ -38,12 +69,9 @@ let counter_Interval = setInterval( function(){
                         $('.loading__page').remove()
                     })
                     $('.hero__banner').animate ({top:'0rem'} )
-                    
                 })
+                $('body').css({'overflow':'auto'})
             });
         })
     }
 }, 200);
-
-
-
