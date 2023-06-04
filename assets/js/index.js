dd521c89d1acc27f75fb306035645860700bc4b2
Yousef-Elkhawanki <yousef.eraasoft@gmail.com>
2023-06-04 22:32:06 +0300

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,32 @@
 // Global Variables 
-const images__list = document.querySelectorAll('.image__container img');
-const text__container = document.querySelector('.text__container h6');
+const images__list = Array.from(document.querySelectorAll('.image__container img:last-child'));
+const text__container = document.querySelectorAll('.text__container h6');
 const text__box = document.querySelector('.text__container');
 let loading__page__counter = document.querySelector('.loading__page span');
 let counter = 0
+let textContent ;
 // stop hover event on text 
 text__box.style.pointerEvents = "none"; 
 
 
 // Event when Move In Or Out All Pictures 
 for (let i = 0; i < images__list.length; i++) {
-    // when Enter Images 
     images__list[i].addEventListener('mouseenter' ,(e)=>{
-        let textContent = e.target.getAttribute ('alt');
-        if (textContent == $('.text__container h6')[0].textContent){
-            console.log($('.text__container h6:nth-child(1)'));
-            $('.text__container h6:nth-child(1)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        $('.welcome__message').animate ({opacity :'0'} , 500)
+        $('.welcome__message').remove ()
+        for (let i = 0; i < images__list.length; i++) {
+            images__list[i].classList.remove('active')
+            
         }
-        else if (textContent == $('.text__container h6')[1].textContent){
-            $('.text__container h6:nth-child(2)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
-        }
-        else if (textContent == $('.text__container h6')[2].textContent){
-            $('.text__container h6:nth-child(3)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
-        }
-        else if (textContent == $('.text__container h6')[3].textContent){
-            $('.text__container h6:nth-child(4)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
-        }
-        else if (textContent == $('.text__container h6')[4].textContent){
-            $('.text__container h6:nth-child(5)').css({opacity:'1' , transform:'translate(-50% , -50%)'})
+        textContent = e.target.getAttribute ('alt');
+        if (images__list[i].getAttribute('alt') == text__container[i].textContent){
+            text__container[i].style.opacity = '1'
         }
     });
-    
         // when Out Images 
     images__list[i].addEventListener('mouseleave' ,(e)=>{
-        let textContent = e.target.getAttribute ('alt');
-        if (textContent == $('.text__container h6')[0].textContent){
-            console.log($('.text__container h6:nth-child(1)'));
-            $('.text__container h6:nth-child(1)').css({opacity:'0', transform:'translate(-50% , 0%)'})
-        }
-        else if (textContent == $('.text__container h6')[1].textContent){
-            $('.text__container h6:nth-child(2)').css({opacity:'0', transform:'translate(-50% , 0%)'})
-        }
-        else if (textContent == $('.text__container h6')[2].textContent){
-            $('.text__container h6:nth-child(3)').css({opacity:'0', transform:'translate(-50% , 0%)'})
-        }
-        else if (textContent == $('.text__container h6')[3].textContent){
-            $('.text__container h6:nth-child(4)').css({opacity:'0', transform:'translate(-50% , 0%)'})
-        }
-        else if (textContent == $('.text__container h6')[4].textContent){
-            $('.text__container h6:nth-child(5)').css({opacity:'0', transform:'translate(-50% , 0%)'})
-        }
-    });
-};
-
-
-
-
+        text__container[i].style.opacity = '0'
+})}
 // counter Function 
 let counter_Interval = setInterval( function(){
     loading__page__counter.textContent = counter+=10
@@ -64,13 +35,10 @@ let counter_Interval = setInterval( function(){
         $(document).ready(()=>{
             $('.loading__container').fadeOut(1000 , ()=>{
                 $('.loading__page').slideUp(1000 , ()=>{
-                    $('.welcome__message').fadeOut(2000 ,()=>{
-                        $('.welcome__message').remove()
                         $('.loading__page').remove()
-                    })
-                    $('.hero__banner').animate ({top:'0rem'} )
+                    $('.hero__banner').animate ({top:'0rem'} ,1000)
                 })
-                $('body').css({'overflow':'auto'})
+                // $('body').css({'overflow':'auto'})
             });
         })
     }
